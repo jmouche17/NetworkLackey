093910bb0668cf99703f65170ad8506ecf86a329
jmouche17 <57845820+jmouche17@users.noreply.github.com>
2026-01-30 21:54:05 -0600

    Update firmware.sh

--- Fortigate/firmware.sh
+++ Fortigate/firmware.sh
@@ -1,10 +1,4 @@
-bash << 'EOF'
-set -e
-
-echo "API_URL=$API_URL"
-[[ -n "$API_TOKEN" ]] && echo "API_TOKEN is set"
-
-curl -sk \
+#!/bin/bash
+pwd
+curl -k \
   -H "Authorization: Bearer $API_TOKEN" \
-  "${API_URL%/}/api/v2/monitor/system/status"
-EOF
